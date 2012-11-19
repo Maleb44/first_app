2df4fb7a5d39ca58618206c8de21d66352c93e4e
Maleb44 <maleb44@yahoo.com>
2012-11-19 14:02:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirtsApp::Application.config.secret_token = 'e6e1d77e386db8fd57cefa2429926539e5554d06831827769262f758dd1bddc9c47a0dae4354b6cf8a5bced1cf26f1fa602202a9d1560a7de7e9fca0a7d9f020'
